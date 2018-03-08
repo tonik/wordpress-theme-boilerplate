3e737e3cce6fd94bfcba9e2fddd454ea25775d09
Grzegorz Rybkowski <gr@tonik.pl>
2018-03-08 17:11:39 +0100

    [templates common] added article as a common component

---
+++ resources/templates/common/common-article.php
@@ -0,0 +1,12 @@
+<article>
+    <header>
+        <h1><?= the_title(); ?></h1>
+        <?= the_post_thumbnail() ?>
+    </header>
+
+    <div>
+        <?= the_content(); ?>
+    </div>
+
+    <footer></footer>
+</article>
