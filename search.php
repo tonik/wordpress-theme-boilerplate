4262456f0542bd861b2b53ee9962c23c1a0b8edb
Grzegorz Rybkowski <gr@tonik.pl>
2018-03-08 15:31:06 +0100

    [root] search.php added

---
+++ search.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template for displaying search result
+ *
+ *
+ * @link https://codex.wordpress.org/Creating_a_Search_Page
+ *
+ * @package Tonik Boilerplate
+ */
+
+get_header();
+
+// content
+get_template_part('template-parts/content/content', 'search');
+
+get_footer();
