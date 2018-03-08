81f08c463b55fe1b3160212d5cd5c7a5388dd4b5
Grzegorz Rybkowski <gr@tonik.pl>
2018-03-08 15:27:15 +0100

    [root] functions.php included generic functions from /inc

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+    include 'include/register-styles.php';
+    include 'include/register-scripts.php';
+
+    include 'include/custom-nav-register.php';
+    include 'include/custom-post-types.php';
+    include 'include/custom-thumbnails.php';
