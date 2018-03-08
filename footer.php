8bdbfdd3c6b92db3624e0a5e852c6be13f28083a
Grzegorz Rybkowski <gr@tonik.pl>
2018-03-08 17:10:29 +0100

    [parts] added footer part

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
-    <footer></footer>
+    <!-- header -->
+    <?php get_template_part('resources/templates/parts/parts', 'footer') ?>
+
     <?php wp_footer(); ?>
 </body>
 </html>
---
+++ resources/templates/parts/parts-footer.php
@@ -0,0 +1,7 @@
+<footer>
+
+    <!-- bottom nav -->
+    <?php get_template_part('resources/templates/nav/nav', 'bottom') ?>
+
+    <span>&copy; 2018</span>
+</footer>
