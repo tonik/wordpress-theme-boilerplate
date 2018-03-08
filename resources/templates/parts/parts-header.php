133539ce7ccd2b9a7f4d238ec43607f116d3188d
Grzegorz Rybkowski <gr@tonik.pl>
2018-03-08 17:11:04 +0100

    [parts] moved <header> into header partial

--- resources/templates/parts/parts-header.php
+++ resources/templates/parts/parts-header.php
@@ -1,7 +1,13 @@
-<div>
-    <!-- logo -->
-    <div>logo</div>
+<header>
 
-    <!-- search form -->
-    <?php get_search_form(); ?>
-</div>
+    <div>
+        <!-- logo -->
+        <div>logo</div>
+
+        <!-- search form -->
+        <?php get_search_form(); ?>
+    </div>
+
+    <!-- top nav -->
+    <?php get_template_part('resources/templates/nav/nav', 'top') ?>
+</header>
