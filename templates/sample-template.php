df5ff596dddfa7e2f956003114c1d2e799be35c1
Grzegorz Rybkowski <gr@tonik.pl>
2018-03-08 15:29:57 +0100

    [templates] added sample-template structure file

---
+++ templates/sample-template.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This is custom template, that you can select from template option when adding new page/post/custom-post.
+ *
+ * To modify template, simply rename this file and change Template Name below
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Tonik Boilerplate
+ */
+
+// Template Name: Sample Template
+get_header();
+
+/**
+ * Content goes here
+ */
+get_template_part('template-parts/content/content', 'sample-template');
+
+get_footer();
+?>
