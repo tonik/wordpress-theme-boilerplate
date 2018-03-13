a3870858f42141af70a70864d4d233079045dbd4
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2018-03-13 11:08:22 +0100

    Move source directory from includes/ to src/

--- functions.php
+++ functions.php
@@ -7,13 +7,13 @@
  |
  | This file is should responsible only for including theme's components.
  | Your theme custom logic should be distributed across
- | separate files in the `/includes` directory.
+ | separate files in the `/src` directory.
  |
  */
 
-include 'include/styles.php';
-include 'include/scripts.php';
+include 'src/styles.php';
+include 'src/scripts.php';
 
-include 'include/nav.php';
-include 'include/post-types.php';
-include 'include/thumbnails.php';
+include 'src/nav.php';
+include 'src/post-types.php';
+include 'src/thumbnails.php';
