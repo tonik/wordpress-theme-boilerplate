f32439059c3403c6e38a0ee308988f9ad5307fe4
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2018-03-21 13:06:17 +0100

    Do not use named template for index content

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <main class="box">
     <?php if ( have_posts() ) : ?>
         <?php if ( is_home() && is_front_page() ) : ?>
-            <?php get_template_part( 'resources/templates/content/content', 'homepage' ); ?>
+            <?php get_template_part( 'resources/templates/content/content' ); ?>
         <?php else : ?>
             <?php while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'resources/templates/content/content', get_post_type() ); ?>
